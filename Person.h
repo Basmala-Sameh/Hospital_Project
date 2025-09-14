ba1be28af2bf7d30aba92d246e887c423f63a4f8
Basmala <basmala.2005sameh@gmail.com>
2025-09-14 16:01:01 +0300

    deleting age attribute from person

--- Person.h
+++ Person.h
@@ -6,21 +6,21 @@ class Person {
 protected:
     int id;
     string name;
-    int age;
+    
 public:
-    Person(int id = 0, string name = "", int age = 0)
-        : id(id), name(name), age(age) {
+    Person(int id = 0, string name = "")
+        : id(id), name(name) {
     }
     virtual void display() const {
-        cout << "ID: " << id << " | Name: " << name << " | Age: " << age;
+        cout << "ID: " << id << " | Name: " << name;
     }
 
     int getId() const { return id; }
     string getName() const { return name; }
-    int getAge() const { return age; }
+    
 
     void setID(const int& i) { id = i; }
     void setName(const string& n) { name = n; }
-    void setAge(int a) { age = a; }
+    
 
 }
