d644dd814d68961770b7ab02fb98c3715b15a1d9
Basmala <basmala.2005sameh@gmail.com>
2025-09-16 18:48:42 +0300

    add to doctor class setters,getters,constructor,attributes

---
+++ Doctor.h
@@ -0,0 +1,25 @@
+#include"Person.h" 
+#include <iostream> 
+#include <string> 
+#include<vector> 
+using namespace std;
+class Doctor :public Person
+{
+protected:
+	string specialization;
+	vector<string>timeSlots;
+
+public:
+	
+	void setSpecilization(string s) { specialization = s; }
+	void set_Time_Slots(vector<string>t) { timeSlots = t; }
+
+	string getSpecilization() const { return specialization; }
+	vector<string>get_Time_Slots()const { return timeSlots; }
+
+	Doctor(int i,string n ,int a, string s, vector<string>t) :Person(i,n,a)
+	{ 
+		specialization = s; 
+		timeSlots = t; 
+	};
+};
